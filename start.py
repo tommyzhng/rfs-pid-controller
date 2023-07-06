f5a3d70b631123efdc548b5c71869d30c9aaaea4
tommyzhng <tommyz2005@gmail.com>
2023-07-06 19:59:19 -0400

    add start.py to start scrcpy & change aspect ratio

---
+++ start.py
@@ -0,0 +1,20 @@
+import os
+import signal
+import sys
+
+
+def signal_handler(sig, frame):
+    print("Ctrl+C pressed! Executing cleanup...")
+    os.system("adb shell wm size reset")
+    sys.exit(0)
+
+# Register the signal handler function
+signal.signal(signal.SIGINT, signal_handler)
+
+adbSize = "adb shell wm size 1080x1920"
+startcmd = "scrcpy --video-codec=h265 --video-bit-rate=16M --audio-bit-rate=256K --max-fps=120"
+
+os.system(adbSize)
+os.system(startcmd)
+
+#cd C:\Users\tommy\Documents\Code\Python\RFS-AI & python start.py
