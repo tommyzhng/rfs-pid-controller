bdc853c4f349c5e057d8e403185b4935d15573e5
tommyzhng <tommyz2005@gmail.com>
2023-07-06 20:03:21 -0400

    add fpm detection, merge two threads

--- main.py
+++ main.py
@@ -2,13 +2,17 @@ import cv2 as cv
 from vision import GetVideo, Text
 import time
 
+
 video_stream = GetVideo().start()
 ocr = Text().start(video_stream)
+time.sleep(.5)
 while True:
-    # Quit condition:
     pressed_key = cv.waitKey(1) & 0xFF
     if pressed_key == ord('q'):
         video_stream.stop()
         ocr.stop()
         break
+
     cv.imshow("test", video_stream.frame)
+    cv.imshow("testw", ocr.altROI)
+    cv.imshow("teste", ocr.fpmROI)
--- vision.py
+++ vision.py
@@ -2,6 +2,10 @@ import cv2 as cv
 from threading import Thread
 import pytesseract
 import math
+import time
+import numpy as np
+import re
+
 class GetVideo(): #get video from another thread to reduce latency
     def __init__(self) -> None:
         self.stream = cv.VideoCapture(2, cv.CAP_DSHOW)
@@ -30,54 +34,88 @@ class Text():
     def __init__(self):
         self.stream = None
         self.stopped = False
-        self.custom_oem=r'digits --psm 7 -c tessedit_char_whitelist=0123456789'
+        self.custom_oem=r'--psm 7 -c tessedit_char_whitelist=-0123456789'
 
         self.lastalt = None
+        self.lastfpm = None
 
+        self.fpm = 0
+        self.array = []
+        self.offset = 0
+
+        #post processing
+        self.pattern = r'^-?\d{1,3}$'
+
+    def altitude(self, frame):
+        x,y,width,height = 579, 668, 42-self.offset, 20
+        self.altROI = frame[y:y+height,x:x+width]
+        self.altROI = self.preprocessing(self.altROI)
+        self.alt = pytesseract.image_to_string(self.altROI, lang='digits', config=self.custom_oem)
+        #self.alt, self.lastalt = self.print_with_errors(self.alt, self.lastalt, 10)
+        self.alt = self.alt.replace("\n",'')
+        self.offset = 9 if int(self.alt) <= 100 else 0
+
+        print(f"alt: {self.alt} fpm: {self.fpm}")
+
+    def verticalSpeed(self, frame):
+        x,y,width,height = 730, 662, 50, 17
+        self.fpmROI = frame[y:y+height,x:x+width]
+        self.fpmROI = self.preprocessing(self.fpmROI)
+        self.fpm = pytesseract.image_to_string(self.fpmROI, lang='digits',config=self.custom_oem)
+        #self.fpm, self.lastfpm = self.print_with_errors(self.fpm, self.lastfpm, 20)
+        self.fpm = self.fpm.replace("\n",'')
+
+    def print_with_errors(self, reading, lastReading, measuringAlt=False):
+        
+        if reading != '':
+            reading = int(reading)
+            if lastReading == None:
+                lastReading = reading
+            if abs(lastReading-reading) <= math.sqrt(constant*lastReading) + 5:
+                lastReading = reading
+            else:
+                reading = str(lastReading) + "DUPLICATE"
+        else:
+            if lastReading != None:
+                reading = lastReading
+        return str(reading), lastReading
+              
+    def preprocessing(self,frame):
+        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
+        width = int(frame.shape[1] * 20)
+        height = int(frame.shape[0] * 20)
+        frame = cv.resize(frame, (width, height))
+        
+        clahe = cv.createCLAHE(clipLimit=1.8, tileGridSize=(8, 8))
+        frame = clahe.apply(frame)
+
+        _, frame = cv.threshold(frame, 115, 255, cv.THRESH_BINARY_INV)
+
+        kernel = cv.getStructuringElement(cv.MORPH_RECT, (5, 5))
+        frame = cv.dilate(frame, kernel, iterations=2)
+        frame = cv.erode(frame, kernel, iterations=2)
+
+        return frame    
+    
     def start(self, video_stream):
-        Thread(target=self.get_text, args=()).start()
+        Thread(target=self.get_alt, args=()).start()
+        Thread(target=self.get_fpm, args=()).start()
         self.stream = video_stream
-
         return self
 
-    def get_text(self):
+    def get_alt(self):
         while not self.stopped:
             if self.stream is not None:
                 frame = self.stream.frame
                 self.altitude(frame)
-
-    def altitude(self, frame):
-        x,y,width,height = 0, 677, 60, 18
-        self.altROI = frame[y:y+height,x:x+width]
-        self.altROI = cv.resize(self.altROI, (0, 0), fx=10, fy=10)
-        self.altROI = self.preprocessing(self.altROI)
-
-        self.alt = pytesseract.image_to_string(self.altROI, config=self.custom_oem)
-        self.alt, self.lastalt = self.print_with_errors(self.alt, self.lastalt)
-        
-    
-    def print_with_errors(self, var, lastvar):
-        if var != '':
-            var = int(var)
-            if lastvar == None:
-                lastvar = var
-            if abs(lastvar-var) <= math.sqrt(10*lastvar) + 5:
-                lastvar = var
-            else:
-                var = lastvar
-        else:
-            if lastvar != None:
-                var = lastvar
-        return var, lastvar
-              
-    def preprocessing(self,frame):
-        frame = cv.threshold(frame, 178, 255, cv.THRESH_BINARY)[1]
-        rect_kernel = cv.getStructuringElement(cv.MORPH_RECT, (1, 1))
-        frame = cv.dilate(frame, rect_kernel, iterations = 1)
-        frame = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        return frame    
+    def get_fpm(self):
+        while not self.stopped:
+            if self.stream is not None:
+                frame = self.stream.frame
+                self.verticalSpeed(frame)
     
     def stop(self):
         self.stopped = True
+
 
 
